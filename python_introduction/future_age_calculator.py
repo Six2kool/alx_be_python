dfda49dfa8f77db80babea3482adc0b3c2a02588
uchennasixtus <uche2sixtus@gmail.com>
2025-11-18 22:00:23 +0000

    cleaned up old python introduction files

--- python_introduction/future_age_calculator.py
+++
@@ -1,3 +0,0 @@
-age = input("How old are you?")
-new_age = int(age) + 27
-print(f"In 2050, you will be {new_age} years old.")
--- python_introduction/while_loop.py
+++
@@ -1,9 +0,0 @@
-secret_number = 7
-guess_count = 0
-guess = 0
-
-while guess != secret_number:
-    guess_count += 1
-    guess = int(input("guesss a number between 1 and 10: "))
-
-print(f"you guessed it in {guess_count} tries!")    
