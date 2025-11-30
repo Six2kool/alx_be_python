2a95fe310d284e39564995af5bc809bc4af28d1a
uchennasixtus <uche2sixtus@gmail.com>
2025-11-30 22:51:14 +0000

    added library

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,51 @@
+# library_management.py
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # starts available (not borrowed)
+
+    def check_out(self):
+        self._is_checked_out = True
+
+    def return_book(self):
+        self._is_checked_out = False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Library:
+    def __init__(self):
+        self._books = []  # secret list of all books
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                print(f"Checked out: {title}")
+                return
+        print(f"Book '{title}' is not available or doesn't exist.")
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                print(f"Returned: {title}")
+                return
+        print(f"Book '{title}' was not checked out.")
+
+    def list_available_books(self):
+        available = [book for book in self._books if book.is_available()]
+        if available:
+            for book in available:
+                print(book)
+        else:
+            print("No books available right now!")
