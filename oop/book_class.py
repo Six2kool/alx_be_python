84e416bbf6cff9a34f2101fb9060d6d6ed6c6e0f
uchennasixtus <uche2sixtus@gmail.com>
2025-12-08 13:32:00 +0000

    deleted a syntax

--- oop/book_class.py
+++ oop/book_class.py
@@ -6,8 +6,7 @@ class Book:
         self.title = title          # save the title inside the book
         self.author = author        # save the author
         self.year = year            # save the year
-        print(f"New book created: {title}")  # optional, just to see it born
-
+        
     # Friendly way to print the book
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
